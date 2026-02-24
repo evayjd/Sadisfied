e3a89738f53b7d2c112a887becdefe3a3f2cd35c
evayjd <yujing.dong2002@gmail.com>
2026-02-24 21:16:33 +0100

    gradio功能

--- app.py
+++ app.py
@@ -1,20 +1,19 @@
 import uuid
+from dotenv import load_dotenv
+load_dotenv()
+
+import gradio as gr
 from langchain_core.messages import HumanMessage
 from graph.builder import build_graph
 
 graph = build_graph()
 
 
-def run_chat():
-    import uuid
-    from langchain_core.messages import HumanMessage
-
-    session_id = str(uuid.uuid4())
-
-    state = {
+def init_state():
+    return {
         "messages": [],
         "user_id": "test_user",
-        "session_id": session_id,
+        "session_id": str(uuid.uuid4()),
         "turn_count": 0,
         "response": None,
         "risk_level": 0,
@@ -23,17 +22,43 @@ def run_chat():
         "emotion": None,
     }
 
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() in ["exit", "quit"]:
-            break
 
-        state["messages"].append(HumanMessage(content=user_input))
-
-        state = graph.invoke(state)
-
-        print("Assistant:", state.get("response"))
+def chat(user_input, state):
+    state["messages"].append(HumanMessage(content=user_input))
+    state = graph.invoke(state)
+    return state.get("response"), state
 
 
-if __name__ == "__main__":
-    run_chat()
+with gr.Blocks() as demo:
+
+    state = gr.State(init_state())
+
+    chatbot = gr.Chatbot()
+    msg = gr.Textbox(placeholder="Type your message...")
+
+    def respond(message, chat_history, state):
+
+        #  调用后端
+        reply, state = chat(message, state)
+
+        # 更新前端聊天记录
+        chat_history.append({
+            "role": "user",
+            "content": message
+        })
+
+        chat_history.append({
+            "role": "assistant",
+            "content": reply
+        })
+
+        # 返回更新
+        return "", chat_history, state
+
+    msg.submit(
+        respond,
+        inputs=[msg, chatbot, state],
+        outputs=[msg, chatbot, state]
+    )
+
+demo.launch()
